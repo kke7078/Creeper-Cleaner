06d17586ab48ee74aed280fcdf29021597a3f633
GANG\82107 <kke7078@naver.com>
2024-10-06 12:53:48 +0900

    플레이어 이동 구현

---
+++ Assets/PROJECT_KGY/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+namespace KGY
+{
+    public class CharacterController : MonoBehaviour
+    {
+        public Vector3 moveInput;
+        public float moveSpeed;
+
+        protected float targetRotation;
+        protected float rotationVelocity;
+        protected float RotationSmoothTime = 0.12f;
+
+        private UnityEngine.CharacterController unityCharacterController;
+
+        private void Awake()
+        {
+            unityCharacterController = GetComponent<UnityEngine.CharacterController>();
+        }
+
+        private void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            moveInput = new Vector3(horizontal, 0, vertical);
+            Move(moveInput);
+        }
+
+        public void Move(Vector3 input)
+        {
+            float magnitude = input.magnitude;
+            if (magnitude <= 0.1f) return;
+
+            Vector3 inputDirection = moveInput.normalized;
+
+            targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref rotationVelocity, RotationSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+            Vector3 targetDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
+
+            unityCharacterController.Move(inputDirection * moveSpeed * Time.deltaTime);
+        }
+    }
+}
