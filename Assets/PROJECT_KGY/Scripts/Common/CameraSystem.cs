310e40766c19a1bef133894ab896180f2d425f36
GANG\82107 <kke7078@naver.com>
2024-10-16 00:05:14 +0900

    Input, Camera System 변경

--- Assets/PROJECT_KGY/Scripts/Common/CameraSystem.cs
+++ Assets/PROJECT_KGY/Scripts/Common/CameraSystem.cs
@@ -16,7 +16,7 @@ namespace KGY
             Vector3 targetPosition = playerPosition + new Vector3(0, 5.5f, -7.05f); //카메라와 캐릭터 사이의 거리
 
             //카메라 이동
-            Camera.main.transform.position = targetPosition;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, targetPosition, Time.deltaTime * cameraSpeed);
         }
     }
 }
--- Assets/PROJECT_KGY/Scripts/Common/InputSystem.cs
+++ Assets/PROJECT_KGY/Scripts/Common/InputSystem.cs
@@ -6,19 +6,34 @@ namespace KGY
 {
     public class InputSystem : MonoBehaviour
     {
+        public static InputSystem Instance;
+
         public CharacterController playerCharacter;
 
-        void OnDrawGizmos()
-        { 
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawLine(playerCharacter.transform.position + Vector3.forward, playerCharacter.transform.position + new Vector3(0, -1f, 2f));
+        public bool isShoot;
+
+        private void Awake()
+        {
+            Instance = this;
         }
 
         private void LateUpdate()
         {
+            if (Input.GetMouseButtonDown(0))
+            { 
+                isShoot = true;
+                playerCharacter.moveSpeed = 3f;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                isShoot = false;
+                playerCharacter.moveSpeed = 5f;
+            }
+
             if (Input.GetMouseButton(0))
             {
-                //클릭한 방향으로 캐릭터 회전
+                //클릭하는 방향으로 캐릭터 회전
                 Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(mouseRay, out RaycastHit hitInfo, 1000f))
                 {
