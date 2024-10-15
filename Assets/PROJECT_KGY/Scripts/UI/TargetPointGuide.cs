ac5da22ed4ee21c29acb4d33fdacfd51694acbc1
GANG\82107 <kke7078@naver.com>
2024-10-15 23:45:22 +0900

    마우스 왼쪽버튼 누르고 있을 때 타겟 오브젝트 줄어들기. 일정 크기 이상 줄어들면 타겟 오브젝트 삭제

---
+++ Assets/PROJECT_KGY/Scripts/UI/TargetPointGuide.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KGY 
+{
+    public class TargetPointGuide : MonoBehaviour
+    {
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (!InputSystem.Instance.isShoot) return;
+
+            if (other.gameObject.layer == LayerMask.NameToLayer("TargetObj"))
+            {
+                if (other.transform.localScale.x > 0.05f)
+                {
+                    other.transform.localScale = Vector3.Lerp(other.transform.localScale, Vector3.zero, Time.deltaTime * 3f);
+                }
+                else
+                {
+                    Destroy(other.gameObject);
+                }
+            }
+        }
+    }
+}
