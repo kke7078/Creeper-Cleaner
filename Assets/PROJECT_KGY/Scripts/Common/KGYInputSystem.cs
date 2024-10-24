789ec424728d3ff1c236afdfb64d51eab969c1dd
GANG\82107 <kke7078@naver.com>
2024-10-25 02:20:36 +0900

    inputSystem 주석 정리

--- Assets/PROJECT_KGY/Scripts/Common/KGYInputSystem.cs
+++ Assets/PROJECT_KGY/Scripts/Common/KGYInputSystem.cs
@@ -47,15 +47,7 @@ namespace KGY
 
         //    if (Input.GetMouseButton(0))
         //    {
-        //        //클릭하는 방향으로 캐릭터 회전
-        //        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //        if (Physics.Raycast(mouseRay, out RaycastHit hitInfo, 1000f))
-        //        {
-        //            Vector3 direction = hitInfo.point - playerCharacter.transform.position;
-        //            Quaternion targetRot = Quaternion.LookRotation(direction);
-        //            targetRot.eulerAngles = new Vector3(0, targetRot.eulerAngles.y, 0);
-        //            playerCharacter.transform.rotation = targetRot;
-        //        }
+        //        
         //    }
         //}
     }
